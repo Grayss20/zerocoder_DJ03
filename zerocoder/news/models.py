3e97d63ed0d7b8151254d5356ec391107c8d42d4
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-10-19 18:37:59 +0100

    hw final

--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User  # Import the User model
 
 # Create your models here.
+class NewsPost(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('дата публикации')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1,
+                               verbose_name='Автор')  # Replace 1 with the ID of the default user
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- zerocoder/news/views.py
+++ zerocoder/news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import NewsPost
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/new.html')
+    news = NewsPost.objects.all()
+    return render(request, 'news/news.html', {'news':  news})
